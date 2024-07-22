50f4694a58a1de23f549ffbb28cefe256516fdcb
savier018 <acosta.savier.03@gmail.com>
2024-07-22 10:54:02 -0500

    haciendo guia 19

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
- /* 1. Importe el módulo crypto */
- let crypto = require('crypto');
+/* 1. Importe el módulo crypto */
+let crypto = require('crypto');
 
 /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
 const sequelize = require('../models/index.js').sequelize;
 var initModels = require("../models/init-models");
-var models = initModels( sequelize );  
+var models = initModels(sequelize);
 
 /* GET users listing. */
 /* 2. Convierta el callback en asíncrono */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
   /* 3. Uso del método findAll */
-  let usersCollection = await models.users.findAll({ })
+  let usersCollection = await models.users.findAll({
+    /* 3.1. Including everything */
+    include: { all: true, nested: true },
+
+    /* 3.2. Raw Queries */
+    raw: true,
+    nest: true,
+
+  })
+
+  let rolesCollection = await models.roles.findAll({})
 
   /* 4. Paso de parámetros a la vista */
-  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
+  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection, rolesArray: rolesCollection });
 
 });
 
- /* POST user. */
- /* 2. Cree el callback asíncrono que responda al método POST */
- router.post('/', async (req, res) => {
+/* POST user. */
+/* 2. Cree el callback asíncrono que responda al método POST */
+router.post('/', async (req, res) => {
 
   /* 3. Desestructure los elementos en el cuerpo del requerimiento */
   let { name, password, idrole } = req.body;
@@ -37,6 +47,8 @@ router.get('/', async function(req, res, next) {
 
     /* 5. Guarde el registro mediante el método create */
     let user = await models.users.create({ name: name, password: passwordHash })
+    /* 5.1. Utilice el model.user_roles para crear la relación ( user.iduser , idrole) */
+    await models.users_roles.create({ users_iduser: user.iduser, roles_idrole: idrole })
 
     /* 6. Redireccione a la ruta con la vista principal '/users' */
     res.redirect('/users')
